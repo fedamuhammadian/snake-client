e951efd50e8622b3e46f6c6f5e27e0bd32117831
fedamuhammadian <f.muhammadian@gmail.com>
2023-07-20 15:56:45 -0700

    now user can send messages to game screen

--- input.js
+++ input.js
@@ -1,25 +1,37 @@
 let moveCommand = "";
 let connection ;
+let cannedMessages = {
+    1: "Say: LOL",
+    2: "Say: OMG",
+    3: "Say: 😘"
+}
 const handleUserInput = (key) => {
     if (key === '\u0003') {
       process.exit();
     }
-  
-    if (key === 'w') {
-      moveCommand = "Move: up";
+    if (key === 'w') { moveCommand = "Move: up";
     } else if (key === 's') {
       moveCommand = "Move: down";
     } else if (key === 'a') {
       moveCommand = "Move: left";
     } else if (key === 'd') {
       moveCommand = "Move: right";
-    }
+    } 
     if (connection && moveCommand) {
         connection.write(moveCommand);
-      }
-  };
+    }
+
+    if (key === '1'&& connection) {
+        connection.write (cannedMessages['1'])
+    } else if (key === '2'&& connection) {
+        connection.write (cannedMessages['2'])
+    } else if (key === '3'&& connection) {
+        connection.write (cannedMessages['3'])
+    };
+};
+
   
-  const setupInput = (conn) => {
+const setupInput = (conn) => {
     connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
@@ -29,6 +41,6 @@ const handleUserInput = (key) => {
     stdin.on("data", handleUserInput);
   
     return stdin;
-  };
-  
-  module.exports = { setupInput };
+};
+
+module.exports = { setupInput };
